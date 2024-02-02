00897a10157be7e6756ff34cdef2284f3b479746
Rex Arockiasamy <rarockiasamy3@cos-4a10678.cos.gatech.edu>
2024-02-02 11:44:11 -0500

    machine agnostic reduction

--- src/ParReduce.jl
+++ src/ParReduce.jl
@@ -12,25 +12,23 @@ struct ParReduce{T} <: ParOperator{T,T,Linear,NonParametric,External}
     ParReduce(comm::MPI.Comm) = new{Float64}(comm)
 end
 
-(A::ParReduce{T})(x::X) where {T,X<:AbstractVector{T}} = MPI.Allreduce(x, MPI.SUM, A.comm)
-(A::ParReduce{T})(x::X) where {T,X<:AbstractArray{T}} = MPI.Allreduce(x, MPI.SUM, A.comm)
-
-function ChainRulesCore.rrule(A::ParReduce{T}, x::X) where {T,X<:AbstractVector{T}}
-    op_out = A(x)
-    function pullback(op)
-        return NoTangent(), op # TODO: Fix this
+function (A::ParReduce{T})(x::X) where {T,X<:AbstractArray{T}}
+    device = get_device(x)
+    if device == "cpu"
+        return MPI.Allreduce(x, MPI.SUM, A.comm)
+    elseif device == "gpu"
+        return MPI.Allreduce(x |> cpu, MPI.SUM, A.comm) |> gpu
     end
-    return op_out, pullback
 end
 
 function ChainRulesCore.rrule(A::ParReduce{T}, x::X) where {T,X<:AbstractArray{T}}
     op_out = A(x)
     function pullback(op)
-        device = get_device(x)
+        device = get_device(op)
         if device == "cpu"
             return NoTangent(), A(op)
         elseif device == "gpu"
-            return NoTangent(), A(op |> gpu) |> cpu
+            return NoTangent(), A(op |> cpu) |> gpu
         end
     end
     return op_out, pullback
